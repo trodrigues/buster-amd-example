b8cad20c36916919de9a3db5f87a01fd79a9a6e6
Tiago Rodrigues <tmcrodrigues@gmail.com>
2012-06-04 01:03:44 +0200

    alternate loading of modules to test

--- script/test/greeter-test.js
+++ script/test/greeter-test.js
@@ -1,8 +1,15 @@
-define(["greeter"], function(greeter) {
+define([], function() {
     buster.testCase("greeter tests", {
+        setUp: function(done){
+            var self = this;
+            require(['greeter'], function(greeter){
+                self.greeter = greeter;
+            });
+        },
+        
         "greets by name" : function() {
             var name = "John Doe";
-            var msg = greeter.greet(name);
+            var msg = this.greeter.greet(name);
             assert.equals("Hello, John Doe!", msg);
         }
     });
